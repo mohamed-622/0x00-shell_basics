ea0d01d08c4ecbecf806b2163ed33063c82dd543
Mohamed <mohamedezghoudi@gmail.com>
2023-09-22 18:27:41 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 #installing flast from pip3 version 2.1.0
-package { 'flask' :
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
   require  => Package['python3-pip'], # Ensure pip3 is installed first
