221107fd06e16a7509158bd71c741547d3c3b61f
mohamed-622 <mohamedezgoudi@gmail.com>
2023-09-22 18:08:07 +0100

    a typo in pip

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #installing flast from pip3 version 2.1.0
 package { 'flask' :
   ensure   => '2.1.0',
-  provider => 'pipe3',
+  provider => 'pip3',
   require  => Package['python3-pip'], # Ensure pip3 is installed first
 }
