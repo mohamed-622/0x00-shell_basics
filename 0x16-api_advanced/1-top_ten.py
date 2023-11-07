fddc4a40c705a381e23d6f55962819a026398b4e
mohamed-622 <mohamedezgoudi@gmail.com>
2023-11-07 22:30:44 +0100

    pep8

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,8 +4,8 @@ function that queries the Reddit API
 and prints the titles of the first 10 hot posts
 listed for a given subreddit.
 """
-
 import requests
+
 
 def top_ten(subreddit):
     """return the top 10 hot posts"""
