50b17a130f47f73ad6c91eadad18cead3e45e86c
mohamed-622 <mohamedezgoudi@gmail.com>
2023-11-07 21:13:05 +0100

    --

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,14 +19,3 @@ def number_of_subscribers(subreddit):
         return response.json().get("data").get("subscribers")
     else:
         return 0
-
-
-# def number_of_subscribers(subreddit):
-#     """Return the number of subscribers"""
-#     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-#     header = {'User-Agent': 'alx'}
-#     response = requests.get(url, headers=header, allow_redirects=False)
-#     if response.status_code == 200:
-#         return response.json().get("data").get("subscribers")
-#     else:
-#         return 0
